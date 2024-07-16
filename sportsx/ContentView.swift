9b5e3e511c573e90d46aaa6c1c8a0020578ab486
renjie <1397253424@qq.com>
2024-07-17 04:46:14 +0800

    repo init as HelloWorld

---
+++ sportsx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sportsx
+//
+//  Created by 任杰 on 2024/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sportsx/sportsxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportsxApp.swift
+//  sportsx
+//
+//  Created by 任杰 on 2024/7/17.
+//
+
+import SwiftUI
+
+@main
+struct sportsxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
